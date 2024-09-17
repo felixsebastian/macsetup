0d91e83c94536c565ced850d9015cfe6f5c800d3
Felix Sebastian <hi@felixsebastian.dev>
2024-09-18 05:29:59 +0700

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+rm -rf $HOME/Code
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+source ~/.oh-my-zsh/tools/uninstall.sh
+rm $HOME/.gitconfig
+rm $HOME/.zshrc
