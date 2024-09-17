39ec82b70df5760a6281c0aeb71dfcdcc0245dce
Felix Sebastian <hi@felixsebastian.dev>
2024-09-18 05:50:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,9 +11,6 @@ mkdir -p $HOME/Code
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 path+=('/opt/homebrew/bin')
 
-# install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # install homebrew packages
 brew install go-task/tap/go-task
 brew install gh go-task node@20
@@ -26,9 +23,12 @@ gh auth login
 gh repo clone felixsebastian/macsetup $HOME/Code
 cp $HOME/Code/macsetup/payloads/gitconfig $HOME/.gitconfig
 cp $HOME/Code/macsetup/payloads/zshrc $HOME/.zshrc
-source $HOME/.zshrc
 
 # install poetry
 python -m venv $HOME/Code/poetry
 $HOME/Code/poetry/bin/pip install -U pip setuptools
 $HOME/Code/poetry/bin/pip install poetry
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source $HOME/.zshrc
