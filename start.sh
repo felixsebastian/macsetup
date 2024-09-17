bec8b8d98d41554c9bbcb1da7d9036c4de2cfb6a
Felix Sebastian <hi@felixsebastian.dev>
2024-09-18 06:12:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -29,6 +29,5 @@ python -m venv $HOME/Code/poetry
 $HOME/Code/poetry/bin/pip install -U pip setuptools
 $HOME/Code/poetry/bin/pip install poetry
 
-
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
