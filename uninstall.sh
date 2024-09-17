871265042a6855d1bf452119028482d13d7571c4
Felix Sebastian <hi@felixsebastian.dev>
2024-09-18 06:21:55 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 
 rm -rf $HOME/Code
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-rm -rf /opt/homebrew
+sudo rm -rf /opt/homebrew
 source ~/.oh-my-zsh/tools/uninstall.sh
 rm $HOME/.gitconfig
 rm $HOME/.zshrc
